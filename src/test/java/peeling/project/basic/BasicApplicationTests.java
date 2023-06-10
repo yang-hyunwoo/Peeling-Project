828ec05632d7fbbeea97de1afabb3ab54d30a9b1
cohouseol <cohouseol@gmail.com>
2023-06-10 14:16:55 +0900

    peeling-project 프로젝트 init

---
+++ src/test/java/peeling/project/basic/BasicApplicationTests.java
@@ -0,0 +1,13 @@
+package peeling.project.basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
