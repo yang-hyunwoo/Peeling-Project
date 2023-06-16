8f843877e15c2a1ab8eb931741c2ffeeba6ed7dc
cohouseol <cohouseol@gmail.com>
2023-06-16 23:00:16 +0900

    토큰에 payload를 aes256을 이용하여 한번더 암호화를 하여 값을 보이지 않게 하였다.

--- src/main/java/peeling/project/basic/config/jwt/JwtProcess.java
+++ src/main/java/peeling/project/basic/config/jwt/JwtProcess.java
@@ -5,44 +5,52 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import lombok.RequiredArgsConstructor;
 import peeling.project.basic.auth.LoginUser;
 import peeling.project.basic.domain.constant.MemberEnum;
 import peeling.project.basic.domain.member.Member;
+import peeling.project.basic.util.Aes256Util;
 
 import java.util.Date;
 import java.util.UUID;
 
+@RequiredArgsConstructor
 public class JwtProcess {
 
     //토큰 생성
     public static String create(LoginUser loginUser) {
+        Aes256Util aes256 = new Aes256Util();
+        String id = aes256.encrypt(loginUser.getMember().getId().toString());
+        String role = aes256.encrypt(loginUser.getMember().getRole().name());
         String jwtToken = JWT.create()
                 .withSubject("bank")
-                .withExpiresAt(new Date(System.currentTimeMillis() +JwtVO.EXPIRATION_TIME))
-                .withClaim("id", loginUser.getMember().getId())
-                .withClaim("role", loginUser.getMember().getRole().name())
+                .withExpiresAt(new Date(System.currentTimeMillis() + JwtVO.EXPIRATION_TIME))
+                .withClaim("id", id)
+                .withClaim("role", role)
                 .sign(Algorithm.HMAC512(JwtVO.SECRET));
         return "Bearer " + jwtToken;
     }
 
     //refresh 토큰 생성
     public static String refresh(LoginUser loginUser) {
-        String uuid = UUID.randomUUID().toString();
+        Aes256Util aes256 = new Aes256Util();
+        String id = aes256.encrypt(loginUser.getMember().getId().toString());
+        String uuid = aes256.encrypt(UUID.randomUUID().toString());
         String jwtToken = JWT.create()
                 .withSubject("bank")
                 .withExpiresAt(new Date(System.currentTimeMillis() +JwtVO.EXPIRATION_TIME*2))
-                .withClaim("id", loginUser.getMember().getId())
+                .withClaim("id", id)
                 .withClaim("refreshToken", uuid)
                 .sign(Algorithm.HMAC512(JwtVO.SECRET));
         return jwtToken;
     }
 
     //토큰 검증 (return 되는 LoginUser 객체를 강제로 시큐리티 세션에 직접 주입)
-    public static LoginUser verify(String token) {
+    public static LoginUser verify(String token)  {
         DecodedJWT decodedJWT = JWT.require(Algorithm.HMAC512(JwtVO.SECRET)).build().verify(token);
-
-        Long id = decodedJWT.getClaim("id").asLong();
-        String role = decodedJWT.getClaim("role").asString();
+        Aes256Util aes256 = new Aes256Util();
+        Long id = Long.parseLong(aes256.decrypt(decodedJWT.getClaim("id").asString()));
+        String role = aes256.decrypt(decodedJWT.getClaim("role").asString());
         Member member = Member.builder().id(id).role(MemberEnum.valueOf(role)).build();
         LoginUser loginUser = new LoginUser(member);
         return loginUser;
@@ -50,6 +58,7 @@ public class JwtProcess {
 
     public static Long verifyRefresh(String token) {
         DecodedJWT decodedJWT = JWT.require(Algorithm.HMAC512(JwtVO.SECRET)).build().verify(token);
-        return decodedJWT.getClaim("id").asLong();
+        Aes256Util aes256 = new Aes256Util();
+        return  Long.parseLong(aes256.decrypt(decodedJWT.getClaim("id").asString()));
     }
 }
---
+++ src/main/java/peeling/project/basic/util/Aes256Util.java
@@ -0,0 +1,49 @@
+package peeling.project.basic.util;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import peeling.project.basic.exception.CustomApiException;
+
+import java.util.Base64;
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+public class Aes256Util {
+
+    public static String alg = "AES/CBC/PKCS5Padding";
+    private final String key = "7bMLmQkJ2oEwwae9usoOxnomqGRcTH8G";
+    private final String iv = key.substring(0, 16); // 16byte
+
+    public String encrypt(String text){
+        try {
+            Cipher cipher = Cipher.getInstance(alg);
+            SecretKeySpec keySpec = new SecretKeySpec(key.getBytes(), "AES");
+            IvParameterSpec ivParamSpec = new IvParameterSpec(iv.getBytes());
+            cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivParamSpec);
+
+            byte[] encrypted = cipher.doFinal(text.getBytes("UTF-8"));
+            return Base64.getEncoder().encodeToString(encrypted);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new CustomApiException("암호화 오류");
+        }
+
+    }
+
+    public String decrypt(String cipherText)  {
+        try {
+            Cipher cipher = Cipher.getInstance(alg);
+            SecretKeySpec keySpec = new SecretKeySpec(key.getBytes(), "AES");
+            IvParameterSpec ivParamSpec = new IvParameterSpec(iv.getBytes());
+            cipher.init(Cipher.DECRYPT_MODE, keySpec, ivParamSpec);
+
+            byte[] decodedBytes = Base64.getDecoder().decode(cipherText);
+            byte[] decrypted = cipher.doFinal(decodedBytes);
+            return new String(decrypted, "UTF-8");
+        } catch (Exception e){
+            throw new CustomApiException("복호화 오류");
+        }
+    }
+
+}
