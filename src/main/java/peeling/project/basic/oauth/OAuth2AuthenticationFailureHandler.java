a43f888ec6cd6d6a48fc440602cf930aaf2c8544
cohouseol <cohouseol@gmail.com>
2023-06-23 17:11:33 +0900

    querydsl library 추가 및 gitignore  q 클래스 제거

--- src/main/java/peeling/project/basic/oauth/OAuth2AuthenticationFailureHandler.java
+++ src/main/java/peeling/project/basic/oauth/OAuth2AuthenticationFailureHandler.java
@@ -3,18 +3,13 @@ package peeling.project.basic.oauth;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.UriComponentsBuilder;
-import peeling.project.basic.auth.LoginUser;
-import peeling.project.basic.config.jwt.JwtProcess;
 
 import java.io.IOException;
 
-import static peeling.project.basic.config.jwt.JwtProcess.CreateCookie;
 
 @Component
 public class OAuth2AuthenticationFailureHandler extends SimpleUrlAuthenticationFailureHandler {
